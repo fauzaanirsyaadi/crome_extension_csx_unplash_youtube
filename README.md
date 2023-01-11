57db341d354b767b3cffd2cbda62a8416f527feb
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2023-01-11 16:02:15 +0700

    initialize code

---
+++ README.md
@@ -0,0 +1,6 @@
+how to run this code
+1. go to chrome://extensions/
+2. turn on developer mode
+3. click on load unpacked
+4. select the folder where you have the manifest.json file
+5. go to youtube.com and see the magic
---
+++ src/content.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("body").append(
+    '<div class="motivator"><img src="https://source.unsplash.com/random/800x600" alt="random image"/><p>Get back to achieving your coding dreams</p></div>'
+  );
+});
